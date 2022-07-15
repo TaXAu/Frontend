1a5ac4b30f3c2c4d31271b1f75fa300a7a66deeb
sperjar <65524316+sperjar@users.noreply.github.com>
2022-07-15 14:24:00 +0800

    feat(ocr): add function button in ocr page

---
+++ packages/renderer/src/components/common/button/AddButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <RoundedButton
+    bg="blue-200/50"
+    size="3rem"
+  >
+    <Add
+      class="transform"
+      fill="blue-600/80"
+      scale="100"
+    />
+  </RoundedButton>
+</template>
+<script lang="ts" setup>
+import Add from '@material-design-icons/svg/round/add.svg';</script>
---
+++ packages/renderer/src/components/ocr/OcrMain.vue
@@ -0,0 +1,14 @@
+<template>
+  <ImgSetView />
+  <div
+    bottom="10"
+    class="float-buttons"
+    fixed="~"
+    flex="~ row"
+    right="10"
+    space="x-4"
+  >
+    <DelButton />
+    <AddButton />
+  </div>
+</template>
