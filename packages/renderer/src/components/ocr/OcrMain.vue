f75aed8af1222ba7ddfff3cb8dfaaca6e67fee6a
sperjar <65524316+sperjar@users.noreply.github.com>
2022-07-25 13:48:32 +0800

    feat: add singal image info entry

--- packages/renderer/src/components/ocr/OcrMain.vue
+++ packages/renderer/src/components/ocr/OcrMain.vue
@@ -12,11 +12,18 @@
     >
       <ImgOverview />
     </div>
+    <div
+      v-if="state.isInPage('img-info')"
+      class="main-components"
+    >
+      <ImgInfoView />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import {stateStore} from '/@/stores/state';
+import ImgInfoView from '/@/components/ocr/ImgInfo/ImgInfoView.vue';
 
 const state = stateStore();
 </script>
