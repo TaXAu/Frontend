6ee5a4dac0ae4ecbb8e970a81fb6ab8497ea516f
sperjar <65524316+sperjar@users.noreply.github.com>
2022-07-25 14:20:17 +0800

    fix: change margin into height

--- packages/renderer/src/components/ocr/OcrTopBar.vue
+++ packages/renderer/src/components/ocr/OcrTopBar.vue
@@ -1,26 +1,30 @@
 <template>
   <div
-    bg="light-400"
     class="img-top-bar"
-    flex="~"
-    gap="x-2"
-    inset="x-0"
-    items="center"
-    m="y-2 x-4"
-    p="x-4"
-    rounded="lg"
-    shadow="sm"
-    select="none"
-    sticky="~"
-    top="2"
-    z="10"
+    p="y-2 x-4"
   >
-    <ChevronLeft
-      class="transform"
-      fill="hover:blue-600/80"
-      scale="100"
-    />
-    <OcrBreadCrumb />
+    <div
+      bg="light-400"
+      flex="~"
+      gap="x-2"
+      h="full"
+      inset="x-0"
+      items="center"
+      p="x-4"
+      rounded="lg"
+      select="none"
+      shadow="sm"
+      sticky="~"
+      top="2"
+      z="10"
+    >
+      <ChevronLeft
+        class="transform"
+        fill="hover:blue-600/80"
+        scale="100"
+      />
+      <OcrBreadCrumb />
+    </div>
   </div>
 </template>
 
