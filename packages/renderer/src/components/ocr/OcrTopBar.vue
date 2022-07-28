7a074e8f80eb73e21195e8d6114763309980d3db
sperjar <65524316+sperjar@users.noreply.github.com>
2022-07-28 16:10:41 +0800

    fix: use position sticky

--- packages/renderer/src/components/ocr/OcrTopBar.vue
+++ packages/renderer/src/components/ocr/OcrTopBar.vue
@@ -2,6 +2,9 @@
   <div
     class="img-top-bar"
     p="y-2 x-4"
+    sticky="~"
+    top="0"
+    z="10"
   >
     <div
       bg="light-400"
@@ -14,9 +17,6 @@
       rounded="lg"
       select="none"
       shadow="sm"
-      sticky="~"
-      top="2"
-      z="10"
     >
       <ChevronLeft
         class="transform"
