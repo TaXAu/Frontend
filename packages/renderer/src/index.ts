7b02a5e438b9a4678460c6e79e1d177cf3c51f59
sperjar <65524316+sperjar@users.noreply.github.com>
2022-08-01 10:16:01 +0800

    fix: fix mutation events undefined in produce env

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,4 +1,4 @@
-import {createApp} from 'vue';
+import {createApp, watch} from 'vue';
 import App from '/@/App.vue';
 import {createPinia} from 'pinia';
 import {router} from '/@/router';
@@ -11,38 +11,8 @@ import './electron/api';
 
 createApp(App).use(createPinia()).use(router).mount('#app');
 
+/* Pinia Store Auto Update */
 const store = await import('/@/stores/state');
 const stateStore = store.stateStore();
-stateStore.$subscribe((mutation, state) => {
-  const events = mutation.events;
-  if ((events as any)?.key) {
-    switch ((events as any)?.key) {
-      case 'prjId':
-        stateStore._updateStorePrjInfo();
-        break;
-
-      case 'imgId':
-        stateStore._updateStoreImgInfo();
-        break;
-
-      case 'changedPrjId':
-        if ((typeof state.ocr.changedPrjId === 'string'
-            && state.ocr.changedPrjId === state.ocr.prjId) ||
-          (state.ocr.changedPrjId instanceof Array
-            && new Set(state.ocr.changedPrjId).has(state.ocr.prjId ?? ''))) {
-          stateStore._updateStoreImgInfo();
-        }
-        break;
-
-      case 'changedImgId':
-        if ((typeof state.ocr.changedImgId === 'string'
-            && state.ocr.changedImgId === state.ocr.imgId) ||
-          (state.ocr.changedImgId instanceof Array
-            && new Set(state.ocr.changedImgId).has(state.ocr.imgId ?? ''))) {
-          stateStore._updateStoreImgInfo();
-        }
-        break;
-    }
-  }
-});
-
+watch(() => stateStore.ocr.prjId, () => stateStore._updateStorePrjInfo());
+watch(() => stateStore.ocr.imgId, () => stateStore._updateStoreImgInfo());
